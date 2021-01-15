388f14c8c88271425f3425671283d8eecc56d559
Ben Kraines <11bkrain@gmail.com>
2021-01-15 15:38:44 -0500

    Added Toggle Button Group

--- server/main.py
+++ server/main.py
@@ -4,10 +4,13 @@ Launches a serving API for a pre-trained generator model.
 
 
 import yaml
+import uvicorn
 from model_wrapper import GeneratorWrapper
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 # parse configuration file
 with open('../config.yaml', 'r') as fp:
@@ -18,18 +21,26 @@ generator_wrapper = GeneratorWrapper(config)
 
 app = FastAPI()
 
+# Serve the React App Frontend
+@app.get('/')
+def index():
+    return FileResponse('../client/build/index.html', media_type='text/html')
+
+app.mount('/static', StaticFiles(directory='../client/build/static'), name='static')
+
 @app.get('/api/summary')
-async def get_summary_dict():
+def get_summary_dict():
     return generator_wrapper.get_summary_dict()
 
 @app.post('/api/generate-image')
-async def generate_image(labels: List[int]):
+def generate_image(labels: List[int]):
     generator_wrapper.generate_image(labels, media_type='image/png')
-
     return FileResponse('gen_out.png', media_type='image/png')
 
 @app.get('/api/sample-new-style', status_code=200)
-async def sample_new_style():
+def sample_new_style():
     generator_wrapper.sample_new_style()
-
     return 'success'
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='localhost', port=8080, reload=True)
