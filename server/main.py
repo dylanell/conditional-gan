e1a34d6cafe5c82664d6b7610131a1b6d2a6c063
dylanell <dellio15@gmail.com>
2021-01-16 14:34:37 -0500

    removed double api dir

--- server/main.py
+++ server/main.py
@@ -8,6 +8,7 @@ from model_wrapper import GeneratorWrapper
 from typing import List
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import uvicorn
 
 # parse configuration file
 with open('../config.yaml', 'r') as fp:
@@ -23,13 +24,15 @@ async def get_summary_dict():
     return generator_wrapper.get_summary_dict()
 
 @app.post('/api/generate-image')
-async def generate_image(labels: List[int]):
+async def generate_image(labels: List[float]):
     generator_wrapper.generate_image(labels, media_type='image/png')
-
     return FileResponse('gen_out.png', media_type='image/png')
 
-@app.get('/api/sample-new-style', status_code=200)
+@app.get('/api/sample-new-style')
 async def sample_new_style():
     generator_wrapper.sample_new_style()
-
     return 'success'
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='localhost', port=8080, reload=False)
--- server/model_wrapper.py
+++ server/model_wrapper.py
@@ -33,11 +33,11 @@ class GeneratorWrapper():
         self._style_dist = torch.distributions.normal.Normal(
             torch.zeros(1, z_dim), torch.ones(1, z_dim))
 
-        # sample style distribution to initialize style vector
-        self._style_vec = self._style_dist.sample()
-
         # set number classes
         self._num_class = num_class
+
+        # sample style distribution to initialize style vector
+        self.sample_new_style()
 
     def get_summary_dict(self):
         return {
@@ -48,15 +48,13 @@ class GeneratorWrapper():
     def sample_new_style(self):
         self._style_vec = self._style_dist.sample()
 
-    def generate_image(self, labels: List[int], media_type='image/png'):
-        # create 'multi-hot' distribution from labels list
-        label_vec = torch.zeros(1, self._num_class)
-        label_vec[0, labels] = 1./float(len(labels))
-        
+    def generate_image(self, logits: List[float], media_type='image/png'):
+        # create 'multi-hot' distribution from logits list
+        label_vec = torch.tensor(logits)
+        label_vec /= torch.sum(label_vec)
+
         # generate sample
         gen_out = self._generator(self._style_vec, label_vec)[0]
-
-        print(gen_out)
 
         # save generated sample to image file
         if media_type == 'image/png':
